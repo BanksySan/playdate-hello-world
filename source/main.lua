aa2c882c772d492b37275b83e5fb42bb63daa953
David Banks <BanksySan@googlemail.com>
2023-08-24 22:17:30 +0100

    Some examples.

--- source/main.lua
+++ source/main.lua
@@ -1,28 +1,103 @@
-import "dvd" -- DEMO
-local dvd = dvd(1, -1) -- DEMO
+import "CoreLibs/object"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
 
-local gfx <const> = playdate.graphics
-local font = gfx.font.new('font/Mini Sans 2X') -- DEMO
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+local geo <const> = pd.geometry
+local point <const> = geo.point
+local vector <const> = geo.vector2D
+local timer <const> = pd.timer
 
-local function loadGame()
-	playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
-	math.randomseed(playdate.getSecondsSinceEpoch()) -- seed for math.random
-	gfx.setFont(font) -- DEMO
+function runSplashScreen()
+
+	class("SplashScreen").extends(gfx.sprite)
+
+	function SplashScreen:init()
+		SplashScreen.super.init(self)
+		local image <const> = gfx.image.new("images/blue-looking-regal")
+
+		gfx.pushContext(image)
+			gfx.setColor(gfx.kColorWhite)
+			gfx.fillRoundRect(263, 9, 89, 25, 3)
+			gfx.drawRoundRect(259, 5, 97, 33, 7)
+			gfx.drawText("Blue's Balls", 267, 12)
+		gfx.popContext()	
+
+		self:setImage(image)
+		self:setCenter(0, 0)
+		self:moveTo(0, 0)
+		self:add()
+	end
+
+	SplashScreen()
+
 end
 
-local function updateGame()
-	dvd:update() -- DEMO
+function runStandardTimer()
+	local callback <const> = function (caller)
+		print ("Tick!")
+		print(caller)
+	end
+
+	local myTimer <const> = timer.new(2000, callback)
+	myTimer.repeats = true
 end
 
-local function drawGame()
-	gfx.clear() -- Clears the screen
-	dvd:draw() -- DEMO
+function runPointAndVectorTests()
+	local a, b <const> = 10, 20
+	local vectors <const> = {
+		v1 = vector.new(a, a),
+		v2 = vector.new(a, a),
+		v3 = vector.new(a, b)
+	}
+
+	local points <const> = {
+		p1 = point.new(a, a),
+		p2 = point.new(a, a),
+		p3 = point.new(a, b)
+	}
+
+	printTable("=== Vectors ===", vectors)
+	print(string.format("%s == %s = %s", vectors.v1, vectors.v2, vectors.v1 == vectors.v2))
+	print(string.format("%s == %s = %s", vectors.v2, vectors.v3, vectors.v2 == vectors.v3))
+	print(string.format("%s == %s = %s", vectors.v3, vectors.v1, vectors.v3 == vectors.v1))
+	print(string.format("%s == %s = %s", vectors.v2, vectors.v1, vectors.v2 == vectors.v1))
+	print(string.format("%s == %s = %s", vectors.v3, vectors.v2, vectors.v3 == vectors.v2))
+	print(string.format("%s == %s = %s", vectors.v1, vectors.v3, vectors.v1 == vectors.v3))
+
+	printTable("=== Points ===", points)
+	print(string.format("%s == %s = %s", points.p1, points.p2, points.p1 == points.p2))
+	print(string.format("%s == %s = %s", points.p2, points.p3, points.p2 == points.p3))
+	print(string.format("%s == %s = %s", points.p3, points.p1, points.p3 == points.p1))
+	print(string.format("%s == %s = %s", points.p2, points.p1, points.p2 == points.p1))
+	print(string.format("%s == %s = %s", points.p3, points.p2, points.p3 == points.p2))
+	print(string.format("%s == %s = %s", points.p1, points.p3, points.p1 == points.p3))
+
+	print("=== Point + vector ===")
+	print(string.format("%s + %s = %s", points.p2, vectors.v1, points.p2 + vectors.v1))
 end
 
-loadGame()
+
+function runTableManipulation()
+	local myArray <const> = {1, 2, 3}
+	printTable(myArray)
+	table.remove(myArray)
+	printTable(myArray)
+end
+
+local EXAMPLES <const> = {
+	SplashScreen = runSplashScreen,
+	VectorTests = runPointAndVectorTests,
+	TableManipulation = runTableManipulation,
+	Timers = {
+		StandardTimer = runStandardTimer
+	}
+}
+
+EXAMPLES.TableManipulation()
 
 function playdate.update()
-	updateGame()
-	drawGame()
-	playdate.drawFPS(0,0) -- FPS widget
+	gfx.sprite.update()
+	timer.updateTimers()
 end
